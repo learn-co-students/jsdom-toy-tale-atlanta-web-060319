922729c31c7980d8d55a1d64ad94ab4e57e7f60c
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-07-18 16:53:55 -0400

    all but the likes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ let addToy = false
 
 // YOUR CODE HERE
 
+document.addEventListener("DOMContentLoaded", getToysAndDisplay)
+
+let form = document.querySelector(".add-toy-form")
+form.addEventListener("submit", handleSubmit)
+
+let button = document.querySelector(".like-btn")
+button.addEventListener("click", handleLike)
+
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
@@ -17,3 +25,122 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+function getToysAndDisplay() {
+	fetch("http://localhost:3000/toys")
+		.then((response) => response.json())
+		.then((jsoArr) => toysDisplay(jsoArr))
+}
+
+function toysDisplay(jsoArr) {
+	
+	jsoArr.forEach((toy) => {
+		let toyCollection = document.querySelector("#toy-collection")
+
+		let toyCard = document.createElement("div")
+		toyCard.className = "card"
+		toyCard.id = toy.id
+		
+		let h2 = document.createElement("h2")
+		h2.innerText = toy.name
+		
+		let image = document.createElement("img")
+		image.className = "toy-avatar"
+		image.src = toy.image
+
+		let likes = document.createElement("p")
+		likes.innerText = toy.likes
+
+		let likebtn = document.createElement("button")
+		likebtn.className = "like-btn"
+		likebtn.innerText = "Like"
+
+		toyCard.appendChild(h2)
+		toyCard.appendChild(image)
+		toyCard.appendChild(likes)
+		toyCard.appendChild(likebtn)
+
+		toyCollection.appendChild(toyCard)
+	})
+}
+
+function handleSubmit(event) {
+	event.preventDefault()
+
+	let newtoy = {
+		name: event.target[0].value,
+		image: event.target[1].value,
+		likes: 0
+	}
+
+	let config = {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body:JSON.stringify(newtoy)
+    }
+
+	fetch("http://localhost:3000/toys", config)
+	.then((response) => response.json())
+	.then((jso) => toysDisplay([jso]))
+
+	.catch((error) => console.log("Error:", error));
+
+	event.target.reset()
+}
+
+function handleLike() {
+	let config = {
+        method: 'PATCH',
+        headers:{
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body:JSON.stringify(newtoy)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
