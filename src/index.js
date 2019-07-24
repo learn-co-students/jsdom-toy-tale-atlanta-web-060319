e3d9fac31f2d55f377e082f6e39836f95352c771
Dylan Harris <dylanharris23@gmail.com>
2019-07-24 10:06:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -14,101 +14,76 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
-document.addEventListener("DOMContentLoaded", setUpPage)
 
-function setUpPage(){
+document.addEventListener("DOMContentLoaded", () => {
+  const allToys = 'http://localhost:3000/toys/'
+  const toyList = document.querySelector('#toy-collection')
+  function init() {
+    fetch(allToys)
+    .then(res => res.json())
+    .then(toys => toys.map(toy => renderToy(toy)))
+  }
+  
+  function renderToy(toy) {
+    toyList.innerHTML += `<div class="card">
+    <h2>${toy.name}</h2>
+    <img class ="toy-avatar" src=${toy.image}>
+    <p> likes: <span>${toy.likes}</span></p>
+    <button data-id=${toy.id} class="like-btn">Like</button>
+    </div>`
+  }
+  document.addEventListener('click', handleClick)
+  document.addEventListener('submit', handleSubmit)
 
-   let form = document.querySelector(".add-toy-form")
-   // // debugger
-   form.addEventListener("submit", handleSubmit)
+  function handleClick(e) {
+    if (e.target.className === "like-btn") {
+      addLikes(e.target)
+    }
+  }
 
-   getAllToys()
-}
+  function addLikes(eventTarget) {
+    // debugger
+    let moreLikes = parseInt(eventTarget.previousElementSibling.firstElementChild.innerText) + 1
+    fetch(allToys + eventTarget.dataset.id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: moreLikes
+      })
+    })
+    .then(res => res.json())
+    .then(eventTarget.previousElementSibling.firstElementChild.innerText = `${moreLikes}`)
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+    let toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    addNewToy(toy)
+  }
 
-function handleSubmit(e){
-  e.preventDefault()
-
- let toy = {
-   name: e.target.name.value,
-   image: e.target.image.value,
-   likes: 0
- }
-  addNewToy(toy)
-  e.target.reset()
-}
-
-function getAllToys(){
- const toys = 'http://localhost:3000/toys'
- fetch(toys)
- .then(response => response.json())
- .then(data => createToyList(data))
-}
-
-function createToyList(toys){
- toys.forEach(toyCards)
-}
-
-function toyCards(toy){
- const div = document.querySelector("#toy-collection")
-
- let divTwo = document.createElement("div")
- divTwo.className = "card "
-
- let name = document.createElement("h2")
- name.innerText = toy.name
-
- let img = document.createElement("img")
- img.className = "toy-avatar"
- img.src = toy.image
-
- let likeButton = document.createElement("button")
-  likeButton.addEventListener("click", handleSubmitLike)
-  likeButton.className = "likeButton"
-  likeButton.innerText = toy.likes + " Likes"
-  likeButton.setAttribute("data-id", toy.id)
-
-//  let likes = document.createElement("p")
-//  likes.innerText = toy.likes
-
- div.appendChild(divTwo)
- divTwo.appendChild(name)
- divTwo.appendChild(img)
- divTwo.appendChild(likeButton)
-//  divTwo.appendChild(likes)
-}
-
- function handleSubmitLike(e){
-   let moreLikes = parseInt(e.target.innerText) + 1
- fetch (`http://localhost:3000/toys/${e.target.dataset.id}`, {
- method: 'PATCH',
- headers: {
-   'Content-Type': 'application/json',
-   'Accept': 'application/json'
- },
- body: JSON.stringify({
-   likes: moreLikes
- })
+  function addNewToy(toy) {
+    fetch(allToys, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toy)
+    })
+    .then(res => res.json())
+    .then(data => renderToy(data))
+    .catch(res => console.log("Error: ", res))
+  }
+  
+  
+  init()
 })
- .then(response => response.json())
- .then(data => {
-   e.target.innerText = `${moreLikes} Likes`
- })
-}
 
-
-
-function addNewToy(toy){
- fetch("http://localhost:3000/toys", {
-     method: "Post",
-     headers: {
-       "Content-Type": "application/json"
-     },
-     body:JSON.stringify(toy)
- })
- .then(res => res.json())
- .then(function(data){
-   toyCards(data)})
- .catch(res => console.log("Error:", res))
-}
 
 // OR HERE!
